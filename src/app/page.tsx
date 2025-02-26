31409556c1f9787a99f8e34ed7e0b0789faecfdc
aasurjya <ahandique8@gmail.com>
2025-02-26 23:59:13 +0530

    Your descriptive commit message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import Header from '../compoents/header';
 
 export default function Home() {
   return (
-    <p>fdffdgdfgdf</p>
+    <>
+      <Header />
+    </>
   );
 }
